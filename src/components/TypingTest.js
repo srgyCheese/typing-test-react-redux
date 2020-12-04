5811e9930a129ed841f58785d1e67a0a88ac2d88
srgyCheese <sergey.sirtsilin@gmail.com>
2020-12-04 18:28:21 +0400

    start

---
+++ src/components/TypingTest.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TypingTest = () => {
+   return (
+      <div className="typing-test">
+         <div className="text">
+            прьььььььььььььььььььа
+         </div>
+         <div className="info">
+            Скорость: <br/>
+            0 зн/мин
+            Точность: <br/>
+            0%
+         </div>
+      </div>
+   )
+}
+
+export default TypingTest
